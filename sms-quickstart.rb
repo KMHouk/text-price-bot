06aeaebf58ac4cd779e4e335dfdb96dd6482d52b
Kevin Houk <kmhouk@gmail.com>
2015-08-15 08:26:56 -0700

    error message improvement

--- sms-quickstart.rb
+++ sms-quickstart.rb
@@ -10,9 +10,9 @@ get '/' do
       r.Message "#{ticker[text].symbol}#{ticker[text].last}"
     elsif text.match(" ")
       space = text.index(" ")
-      r.Message "#{Blockchain::to_btc(text[space+1..-1], text[0..space-1])}"
+      r.Message "#{Blockchain::to_btc(text[space+1..-1], text[0..space-1])} BTC"
     else
-      r.Message "Error"
+      r.Message "Please text the 3 letter currency code for the latest price (ex: 'usd') OR text the currency value, space, 3 letter currency code (ex: '100 usd')"
     end
   end
   twiml.text
