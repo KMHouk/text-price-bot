b3242ef8fba8031ce0816ce0dc0d572f30ec182a
Kevin Houk <kmhouk@gmail.com>
2015-08-15 07:32:14 -0700

    changing case to if statement

--- sms-quickstart.rb
+++ sms-quickstart.rb
@@ -6,10 +6,9 @@ get '/' do
   ticker = Blockchain::get_ticker()
   twiml = Twilio::TwiML::Response.new do |r|
     text = params[:Body]
-    case text
-    when "USD".casecmp(text) == 0
+    if "USD".casecmp(text) == 0
       r.Message ticker["USD"].last
-    when "JPY".casecmp(text) == 0
+    elsif "JPY".casecmp(text) == 0
       r.Message ticker["JPY"].last
     else
       r.Message "Please just send the 3 letter currency code."
