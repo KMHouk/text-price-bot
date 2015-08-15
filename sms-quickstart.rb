9dc89d09b211ff6b7957b9ff32e79ab601948d85
Kevin Houk <kmhouk@gmail.com>
2015-08-15 06:52:13 -0700

    added blockchain component

--- sms-quickstart.rb
+++ sms-quickstart.rb
@@ -1,9 +1,12 @@
 require 'twilio-ruby'
 require 'sinatra'
+require 'blockchain'
 
 get '/' do
+  ticker = Blockchain::get_ticker()
   twiml = Twilio::TwiML::Response.new do |r|
-    r.Message "Hey Monkey. Thanks for the message!"
+    r.Message ticker[params[:Body]].last
+    # r.Message "Hey Monkey. Thanks for the message!"
   end
   twiml.text
 end
